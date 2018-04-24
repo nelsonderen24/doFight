fc960f636f85d79791316c14d019009fbbcc51f5
Lapastoure Corentin <Lapastoure@users.noreply.github.com>
2018-04-24 15:36:49 +0200

    fdp

--- src/jpu2016/dogfight/gameframe/GameFrame.java
+++ src/jpu2016/dogfight/gameframe/GameFrame.java
@@ -3,8 +3,14 @@ package jpu2016.dogfight.gameframe;
 import java.awt.event.KeyEvent;
 import java.util.Observable;
 
+import javax.swing.JFrame;
+
 public class GameFrame extends JFrame {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -118523942411776538L;
 	private IEventPerformer eventPerformer;
 
 	public GameFrame(String title, IEventPerformer performer, IGraphicsBuilder GraphicsBuilder, Observable observable) {
